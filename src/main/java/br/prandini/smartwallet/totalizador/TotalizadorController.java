358d2646925dd96be754a4dbbe7a91e03b3905f6
KaioOliveira <kaio.oliveira@maps.com.br>
2024-05-10 17:46:35 -0300

    feat: endpoint de Totalizador por Conta adicionado

--- src/main/java/br/prandini/smartwallet/totalizador/TotalizadorController.java
+++ src/main/java/br/prandini/smartwallet/totalizador/TotalizadorController.java
@@ -10,6 +10,7 @@ import br.prandini.smartwallet.totalizador.service.TotalizadorService;
 import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class TotalizadorController {
     }
 
     @GetMapping("/balanco/{conta}")
-    public ResponseEntity<TotalizadorBalancoOutput> getTotalizadorBalancoByConta(String conta){
+    public ResponseEntity<TotalizadorBalancoOutput> getTotalizadorBalancoByConta(@PathVariable String conta){
         return ResponseEntity.ok().body(service.getBalancoByConta(conta));
     }
 }
